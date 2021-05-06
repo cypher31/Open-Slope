ce5041f3bce9b4e5cf542a0ae4583876d1c8a08c
Kelby Styler <kelbystyler@gmail.com>
2021-05-06 05:57:35 -0700

    Prototype

---
+++ slope_geometry/slope_geometry.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+#script which handles displaying the slop geometry and relevant details
+#For now, assume that el. 0 is center of the container
+#making sure to scale things correctly will be interesting, camera to control zoom?
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
