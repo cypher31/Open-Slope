16f4a33c7d25c8d7edef9abe68961b06f89b170e
Kelby Styler <kelbystyler@gmail.com>
2021-05-15 13:42:44 -0700

    Continue theta min and max calc

--- global/search_circular.gd
+++ global/search_circular.gd
@@ -71,4 +71,54 @@ func _calculate_theta_increment(termination_point : Position2D, segment_initial 
 	#draw a line perpindicular to each segment starting from the calculated centers and check where they intercept
 	#the distance from the intersection point to the initiation point is the radius of the circle
 	var theta_increment : float #max or min depending on inputs
+	var segment_termination : Line2D = Line2D.new()
+	var segment_initial_points : PoolVector2Array = segment_initial.get_points()
+	
+	segment_termination.set_points([initiation_point.position,termination_point.position]) #segment start point & end
+	var segment_termination_points = segment_termination.get_points()
+	
+	var segment_initial_center_point : Vector2 = _calculate_segment_mid_point(segment_initial)
+	var segment_termination_center_point : Vector2 = _calculate_segment_mid_point(segment_termination)
+	
+	var segment_initial_slope_x = segment_initial_points[1].x - segment_initial_points[0].x
+	var segment_initial_slope_y = segment_initial_points[1].y - segment_initial_points[0].y
+	var segment_initial_slope : Vector2 = Vector2(segment_initial_slope_x, segment_initial_slope_y)
+	
+	var segment_termination_slope_x = segment_termination_points[1].x - segment_termination_points[0].x
+	var segment_termination_slope_y = segment_termination_points[1].y - segment_termination_points[0].y
+	var segment_termination_slope : Vector2 = Vector2(segment_termination_slope_x, segment_termination_slope_y)
+	
+	var segment_initial_perpendicular_slope : Vector2 = segment_initial_slope * - 1
+	var segement_termination_perpendicular_slope :Vector2 = segment_termination_slope * - 1
+	
+	var segment_perpendicular_initial : Line2D
+	var segment_perpendicular_termination : Line2D
+	
+	var segment_perpendicular_initial_start : Vector2 = segment_initial_center_point
+	var segment_perpendicular_termination_start : Vector2 = segment_termination_center_point
+	
+#	var x_point_initial : float = 5000 #the point where the perpendicular segment is ending
+#	var x_point_termination : float = 0
+#	var y_point_initial : float = (segment_initial_slope.y / segment_initial_slope.x)
+#	var y_point_termination : float
+	
+	#I think the problem here is that the vector2 is just a point and it needs to be a full line??
+	var segment_perpendicular_initial_end : Vector2 = segment_perpendicular_initial_start * 1000
+	var segment_perpendicular_termination_end : Vector2 = segment_perpendicular_termination_start * (-1000)
+	
+	var circle_center_point = Geometry.segment_intersects_segment_2d(segment_perpendicular_initial_start, segment_perpendicular_initial_end,segment_perpendicular_termination_start, segment_perpendicular_termination_end)
+#	print(segment_initial_center_point)
+#	print(segment_termination_center_point)
+	print(circle_center_point)
 	return theta_increment
+	
+
+func _calculate_segment_mid_point(segment_initial):
+	var mid_point : Vector2
+	var segment_points = segment_initial.get_points()
+	
+	var x_average = (segment_points[0].x + segment_points[1].x) / 2
+	var y_average = (segment_points[0].y + segment_points[1].y) / 2
+	mid_point = Vector2(x_average, y_average)
+	
+	return mid_point
